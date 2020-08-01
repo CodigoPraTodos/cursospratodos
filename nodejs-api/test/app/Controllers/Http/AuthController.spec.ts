ea3d438eb684d8cb1b443b3121cd3558d4e4eada
José Eduardo <josee.edumont@gmail.com>
2020-08-01 18:49:22 -0300

    Added tests for /auth/login from AuthController

--- nodejs-api/test/app/Controllers/Http/AuthController.spec.ts
+++ nodejs-api/test/app/Controllers/Http/AuthController.spec.ts
@@ -1,5 +1,6 @@
 import test from 'japa'
 import supertest from 'supertest'
+import User from 'App/Models/User'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}/auth`
 
@@ -17,6 +18,24 @@ test.group('AuthController', () => {
       assert.isDefined(response.body.auth.token)
       assert.equal(response.body.user.id, 1)
       assert.notExists(response.body.user.password)
+    })
+
+    test('POST /login', async (assert) => {
+      const user = await User.create({
+        name: 'testename',
+        email: 'teste@email.com',
+        password: 'secret',
+      })
+
+      const response = await supertest(BASE_URL)
+        .post('/login')
+        .send({
+          email: user.$attributes.email,
+          password: 'secret',
+        })
+        .expect(200)
+      assert.isDefined(response.body.auth.token)
+      assert.equal(response.body.user.is_active, true)
     })
   })
 })
