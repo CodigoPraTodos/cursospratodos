da6c982f7fb029d62e9deaf43bf38636b74b6ef3
José Eduardo <josee.edumont@gmail.com>
2020-08-04 21:40:16 -0300

    Added test to logout with valid and invalid token

--- nodejs-api/test/app/Controllers/Http/AuthController.spec.ts
+++ nodejs-api/test/app/Controllers/Http/AuthController.spec.ts
@@ -37,12 +37,31 @@ test.group('AuthController', () => {
       const response = await supertest(BASE_URL)
         .post('/login')
         .send({
-          email: user.$attributes.email,
+          email: user.email,
           password: 'secret',
         })
         .expect(200)
+
       assert.isDefined(response.body.auth.token)
       assert.equal(response.body.user.is_active, true)
+    })
+
+    test('GET /logout with valid token', async (assert) => {
+      const user = await User.create({
+        name: faker.name.findName(),
+        email: faker.internet.email(),
+        password: 'secret',
+      })
+
+      const loginResponse = await supertest(BASE_URL).post('/login').send({ email: user.email, password: 'secret' })
+
+      await supertest(BASE_URL)
+        .get('/logout')
+        .set({ Authorization: `${loginResponse.body.auth.type} ${loginResponse.body.auth.token}` })
+        .expect(204)
+    })
+    test('GET /logout with invalid token', async (assert) => {
+      await supertest(BASE_URL).get('/logout').set({ Authorization: 'token' }).expect(401)
     })
   })
 })
