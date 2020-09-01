9db6514e88ecc710678a86c0fe29e1fd7472046c
vazgabriel <gabrielcvaz@outlook.com>
2020-09-01 17:34:25 -0500

    fix wrong text

--- nodejs-api/database/seeders/Course.ts
+++ nodejs-api/database/seeders/Course.ts
@@ -64,7 +64,7 @@ export default class CourseSeeder extends BaseSeeder {
             : undefined,
         })
       }
-    }youtubeIds
+    }
 
     exercises = await CourseClassExercise.createMany(exercises)
 
